cb5881df4b365bf2a47d528ed3665787325f49a8
Ashlyn <ashbtw19937@gmail.com>
2024-03-28 08:02:31 -0600

    finish string impl for now (probably forgetting something)

--- src/acommon/a_string.c
+++ src/acommon/a_string.c
@@ -1,6 +1,28 @@
 #include "acommon.h"
 
 #include <stdlib.h>
+#include <string.h>
+
+int A_memcmp(const void* a, const void* b, size_t n) {
+    return memcmp(a, b, n);
+}
+
+void* A_memcpy(void* dest, const void* src, size_t n) {
+    return memcpy(dest, src, n);
+}
+
+void* A_memchr(const void* p, int c, size_t n) {
+    return memchr(p, c, n);
+}
+
+void* A_memrchr(const void* p, int c, size_t n) {
+    for(int i = n; i > 0; i--) {
+        if(((char*)p)[i] == (char)c)
+            return (void*)((const char*)p + i);
+    }
+
+    return NULL;
+}
 
 str_t A_str_Literal(const char* s, size_t c) {
     str_t n;
@@ -13,13 +35,6 @@ str_t A_str_String(string_t s) {
     str_t n;
     n.__data = s.__data;
     n.__len  = s.__len;
-    return n;
-}
-
-string_t A_string_Empty(void) {
-    string_t n;
-    n.__data = NULL;
-    n.__len  = 0;
     return n;
 }
 
@@ -59,13 +74,21 @@ size_t A_strlen_String(string_t s) {
     return s.__len;
 }
 
+char A_strat_Str(str_t s, size_t i) {
+    return A_cstr(s)[i];
+}
+
+char A_strat_String(string_t s, size_t i) {
+    return A_cstr(s)[i];
+}
+
 bool A_strcpyz_Str(string_t dest, size_t dest_off, str_t src, size_t src_off) {
     size_t c = A_strlen(src) - src_off;
 
     if(dest_off + c > A_strlen(dest))
         return false;
 
-    memcpy(dest.__data, + dest_off, src.__data + src_off, c);
+    memcpy(dest.__data + dest_off, src.__data + src_off, c);
 
     dest.__data[A_strlen(dest)] = '\0';
 
@@ -80,7 +103,7 @@ bool A_strcpyz_String(
     if(dest_off + c > A_strlen(dest))
         return false;
 
-    memcpy(dest.__data, + dest_off, src.__data + src_off, c);
+    memcpy(dest.__data + dest_off, src.__data + src_off, c);
 
     dest.__data[A_strlen(dest)] = '\0';
 
@@ -113,7 +136,7 @@ bool A_strext(string_t s, size_t n) {
 bool A_strcat_Str(string_t dest, str_t src) {
     size_t off = A_strlen(dest);
 
-    if(!A_strext(dest))
+    if(!A_strext(dest, A_strlen(src)))
         return false;
 
     A_strcpyz(dest, off, src, 0);
@@ -123,7 +146,7 @@ bool A_strcat_Str(string_t dest, str_t src) {
 bool A_strcat_String(string_t dest, string_t src) {
     size_t off = A_strlen(dest);
 
-    if(!A_strext(dest))
+    if(!A_strext(dest, A_strlen(src)))
         return false;
 
     A_strcpyz(dest, off, src, 0);
@@ -158,6 +181,14 @@ size_t A_strrchr_String(string_t s, char c) {
     return (size_t)(p - A_cstr(s));
 }
 
+bool A_strcont_Str(str_t s, char c) {
+    return A_strchr(s, c) != A_NPOS;
+}
+
+bool A_strcont_String(string_t s, char c) {
+    return A_strchr(s, c) != A_NPOS;
+}
+
 bool A_streq_Str_Str(str_t a, str_t b) {
     return A_cstr(a) == A_cstr(b);
 }
@@ -177,7 +208,7 @@ bool A_streq_String_String(string_t a, string_t b) {
 size_t A_strpbrk_Str_Str(str_t a, str_t b) {
     for(int i = 0; i < A_strlen(a); i++) {
         for(int j = 0; j < A_strlen(b); j++) {
-            if(A_strat(a, i) == A_strat(b, j)
+            if(A_strat(a, i) == A_strat(b, j))
                 return (size_t)i;
         }
     }
@@ -188,7 +219,7 @@ size_t A_strpbrk_Str_Str(str_t a, str_t b) {
 size_t A_strpbrk_Str_String(str_t a, string_t b) {
     for(int i = 0; i < A_strlen(a); i++) {
         for(int j = 0; j < A_strlen(b); j++) {
-            if(A_strat(a, i) == A_strat(b, j)
+            if(A_strat(a, i) == A_strat(b, j))
                 return (size_t)i;
         }
     }
@@ -199,7 +230,7 @@ size_t A_strpbrk_Str_String(str_t a, string_t b) {
 size_t A_strpbrk_String_Str(string_t a, str_t b) {
     for(int i = 0; i < A_strlen(a); i++) {
         for(int j = 0; j < A_strlen(b); j++) {
-            if(A_strat(a, i) == A_strat(b, j)
+            if(A_strat(a, i) == A_strat(b, j))
                 return (size_t)i;
         }
     }
@@ -210,7 +241,139 @@ size_t A_strpbrk_String_Str(string_t a, str_t b) {
 size_t A_strpbrk_String_String(string_t a, string_t b) {
     for(int i = 0; i < A_strlen(a); i++) {
         for(int j = 0; j < A_strlen(b); j++) {
-            if(A_strat(a, i) == A_strat(b, j)
+            if(A_strat(a, i) == A_strat(b, j))
+                return (size_t)i;
+        }
+    }
+
+    return A_NPOS;
+}
+
+size_t A_strrpbrk_Str_Str(str_t a, str_t b) {
+    for(int i = A_strlen(a); i > 0; i--) {
+        for(int j = A_strlen(b); j > 0; j--) {
+            if(A_strat(a, i) == A_strat(b, j))
+                return (size_t)i;
+        }
+    }
+
+    return A_NPOS;
+}
+
+size_t A_strrpbrk_Str_String(str_t a, string_t b) {
+    for(int i = A_strlen(a); i > 0; i--) {
+        for(int j = A_strlen(b); j > 0; j--) {
+            if(A_strat(a, i) == A_strat(b, j))
+                return (size_t)i;
+        }
+    }
+
+    return A_NPOS;
+}
+
+size_t A_strrpbrk_String_Str(string_t a, str_t b) {
+    for(int i = A_strlen(a); i > 0; i--) {
+        for(int j = A_strlen(b); j > 0; j--) {
+            if(A_strat(a, i) == A_strat(b, j))
+                return (size_t)i;
+        }
+    }
+
+    return A_NPOS;
+}
+
+size_t A_strrpbrk_String_String(string_t a, string_t b) {
+    for(int i = A_strlen(a); i > 0; i--) {
+        for(int j = A_strlen(b); j > 0; j--) {
+            if(A_strat(a, i) == A_strat(b, j))
+                return (size_t)i;
+        }
+    }
+
+    return A_NPOS;
+}
+
+size_t A_strpcnt_Str_Str(str_t a, str_t b) {
+    for(int i = 0; i < A_strlen(a); i++) {
+        for(int j = 0; j < A_strlen(b); j++) {
+            if(A_strat(a, i) != A_strat(b, j))
+                return (size_t)i;
+        }
+    }
+
+    return A_NPOS;
+}
+
+size_t A_strpcnt_Str_String(str_t a, string_t b) {
+    for(int i = 0; i < A_strlen(a); i++) {
+        for(int j = 0; j < A_strlen(b); j++) {
+            if(A_strat(a, i) != A_strat(b, j))
+                return (size_t)i;
+        }
+    }
+
+    return A_NPOS;
+}
+
+size_t A_strpcnt_String_Str(string_t a, str_t b) {
+    for(int i = 0; i < A_strlen(a); i++) {
+        for(int j = 0; j < A_strlen(b); j++) {
+            if(A_strat(a, i) != A_strat(b, j))
+                return (size_t)i;
+        }
+    }
+
+    return A_NPOS;
+}
+
+size_t A_strpcnt_String_String(string_t a, string_t b) {
+    for(int i = 0; i < A_strlen(a); i++) {
+        for(int j = 0; j < A_strlen(b); j++) {
+            if(A_strat(a, i) != A_strat(b, j))
+                return (size_t)i;
+        }
+    }
+
+    return A_NPOS;
+}
+
+size_t A_strrpcnt_Str_Str(str_t a, str_t b) {
+    for(int i = A_strlen(a); i > 0; i--) {
+        for(int j = A_strlen(b); j > 0; j--) {
+            if(A_strat(a, i) != A_strat(b, j))
+                return (size_t)i;
+        }
+    }
+
+    return A_NPOS;
+}
+
+size_t A_strrpcnt_Str_String(str_t a, string_t b) {
+    for(int i = A_strlen(a); i > 0; i--) {
+        for(int j = A_strlen(b); j > 0; j--) {
+            if(A_strat(a, i) != A_strat(b, j))
+                return (size_t)i;
+        }
+    }
+
+    return A_NPOS;
+}
+
+size_t A_strrpcnt_String_Str(string_t a, str_t b) {
+    for(int i = A_strlen(a); i > 0; i--) {
+        for(int j = A_strlen(b); j > 0; j--) {
+            if(A_strat(a, i) != A_strat(b, j))
+                return (size_t)i;
+        }
+    }
+
+    return A_NPOS;
+}
+
+size_t A_strrpcnt_String_String(string_t a, string_t b) {
+    for(int i = A_strlen(a); i > 0; i--) {
+        for(int j = A_strlen(b); j > 0; j--) {
+            if(A_strat(a, i) != A_strat(b, j))
                 return (size_t)i;
         }
     }
@@ -288,4 +451,134 @@ size_t A_strstr_String_String(string_t a, string_t b) {
     }
 
     return A_NPOS;
+}
+
+size_t A_strrstr_Str_Str(str_t a, str_t b) {
+    if(A_strlen(b) > A_strlen(a))
+        return A_NPOS;
+
+    if(A_strlen(b) == A_strlen(b))
+        return A_streq(a, b);
+
+    char c = A_strat(b, 0);
+    for(int i = A_strlen(a); i > A_strlen(a) - A_strlen(b); i--) {
+        if(A_strat(a, i) == c) {
+            if(A_memcmp(A_cstr(a), A_cstr(b), A_strlen(b)) == 0)
+                return i;
+        }
+    }
+
+    return A_NPOS;
+}
+
+size_t A_strrstr_Str_String(str_t a, string_t b) {
+    if(A_strlen(b) > A_strlen(a))
+        return A_NPOS;
+
+    if(A_strlen(b) == A_strlen(b))
+        return A_streq(a, b);
+
+    char c = A_strat(b, 0);
+    for(int i = A_strlen(a); i > A_strlen(a) - A_strlen(b); i--) {
+        if(A_strat(a, i) == c) {
+            if(A_memcmp(A_cstr(a), A_cstr(b), A_strlen(b)) == 0)
+                return i;
+        }
+    }
+
+    return A_NPOS;
+}
+
+size_t A_strrstr_String_Str(string_t a, str_t b) {
+    if(A_strlen(b) > A_strlen(a))
+        return A_NPOS;
+
+    if(A_strlen(b) == A_strlen(b))
+        return A_streq(a, b);
+
+    char c = A_strat(b, 0);
+    for(int i = A_strlen(a); i > A_strlen(a) - A_strlen(b); i--) {
+        if(A_strat(a, i) == c) {
+            if(A_memcmp(A_cstr(a), A_cstr(b), A_strlen(b)) == 0)
+                return i;
+        }
+    }
+
+    return A_NPOS;
+}
+
+size_t A_strrstr_String_String(string_t a, string_t b) {
+    if(A_strlen(b) > A_strlen(a))
+        return A_NPOS;
+
+    if(A_strlen(b) == A_strlen(b))
+        return A_streq(a, b);
+
+    char c = A_strat(b, 0);
+    for(int i = A_strlen(a); i > A_strlen(a) - A_strlen(b); i--) {
+        if(A_strat(a, i) == c) {
+            if(A_memcmp(A_cstr(a), A_cstr(b), A_strlen(b)) == 0)
+                return i;
+        }
+    }
+
+    return A_NPOS;
+}
+
+str_t A_substr_Str(str_t s, size_t i) {
+    str_t n;
+
+    if(i >= A_strlen(s)) {
+        n.__data = NULL;
+        n.__len  = 0;
+        return n;
+    }
+
+    n.__data = s.__data + i;
+    n.__len  = s.__len  - i;    
+    return n;
+}
+
+str_t A_substr_String(string_t s, size_t i) {
+    str_t n;
+
+    if(i >= A_strlen(s)) {
+        n.__data = NULL;
+        n.__len  = 0;
+        return n;
+    }
+
+    n.__data = s.__data + i;
+    n.__len  = s.__len  - i;    
+    return n;
+}
+
+string_t A_substring_Str(str_t s, size_t i) {
+    string_t n;
+
+    if(i >= A_strlen(s)) {
+        n.__data = NULL;
+        n.__len  = 0;
+        return n;
+    }
+
+    size_t len = A_strlen(s) - i;
+    A_strext(n, len);
+    A_strcpyz(n, 0, s, i);
+    return n;
+}
+
+string_t A_substring_String(string_t s, size_t i) {
+    string_t n;
+
+    if(i >= A_strlen(s)) {
+        n.__data = NULL;
+        n.__len  = 0;
+        return n;
+    }
+
+    size_t len = A_strlen(s) - i;
+    A_strext(n, len);
+    A_strcpyz(n, 0, s, i);
+    return n;
 }
--- src/acommon/acommon.h
+++ src/acommon/acommon.h
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include <stddef.h>
 #include <limits.h>
 
@@ -24,51 +25,57 @@ str_t A_str_String(string_t s);
     string_t:    A_str_String(s) \
 ))
 
-string_t A_string_Empty(void);
 string_t A_string_Str(str_t s);
 string_t A_string_String(string_t s);
-string_t A_string_SizeT(size_t c) {
+string_t A_string_SizeT(size_t c);
 
 #define A_string(s) (_Generic((s), \
-    void:     A_str_Empty(), \
-    str_t:    A_string_Str(s), \
-    string_t: A_string_String(s) \
-    size_t:   A_string_SizeT(s) \
-))
+    size_t:   A_string_SizeT, \
+    str_t:    A_string_Str, \
+    string_t: A_string_String \
+))(s)
 
 const char* A_cstr_Str(str_t s);
 const char* A_cstr_String(string_t s);
 
 #define A_cstr(s) (_Generic((s), \
-    str_t:    A_cstr_Str(s), \
-    string_t: A_cstr_String(s), \
-))
+    str_t:    A_cstr_Str, \
+    string_t: A_cstr_String \
+))(s)
 
-size_t A_strlen_Str(str_t s):
+char A_strat_Str(str_t s, size_t i);
+char A_strat_String(string_t s, size_t i);
+
+#define A_strat(s, i) (_Generic((s), \
+    str_t:    A_strat_Str, \
+    string_t: A_strat_String \
+))(s, i)
+
+size_t A_strlen_Str(str_t s);
 size_t A_strlen_String(string_t s);
 
 #define A_strlen(s) (_Generic((s), \
-    str_t:    A_strlen_Str(s), \
-    string_t: A_strlen_String(s), \
-))
+    str_t:    A_strlen_Str, \
+    string_t: A_strlen_String \
+))(s)
 
 bool A_strcpyz_Str(string_t dest, size_t dest_off, str_t src, size_t src_off);
 bool A_strcpyz_String(
     string_t dest, size_t dest_off, string_t src, size_t src_off
 );
 
-#define A_strcpyz(d, do, s, so) (_Generic((s), \
-    str_t:    A_strcpyz_Str(d, do, s, so), \
-    string_t: A_strcpyz_String(d, do, s, so), \
-))
+#define A_strcpyz(d, do, s, so) _Generic(s, \
+    str_t:    A_strcpyz_Str, \
+    string_t: A_strcpyz_String \
+)((d), (do), (s), (so))
 
 string_t A_strdup_Str(str_t s);
 string_t A_strdup_String(string_t s);
 
 #define A_strdup(s) (_Generic((s), \
-    str_t:    A_strdup_Str(s), \
-    string_t: A_strdup_String(s), \
-))
+    str_t:    A_strdup_Str, \
+    string_t: A_strdup_String \
+))(s)
 
 bool A_strext(string_t s, size_t n);
 
@@ -76,82 +83,143 @@ bool A_strcat_Str(string_t dest, str_t src);
 bool A_strcat_String(string_t dest, string_t src);
 
 #define A_strcat(d, s) (_Generic((s), \
-    str_t:    A_strdup_Str(d, s), \
-    string_t: A_strdup_String(d, s), \
-))
+    str_t:    A_strdup_Str, \
+    string_t: A_strdup_String \
+))((d), (s))
 
 size_t A_strchr_Str(str_t s, char c);
 size_t A_strchr_String(string_t s, char c);
 
 #define A_strchr(s, c) (_Generic((s), \
-    str_t:    A_strchr_Str(s, c), \
-    string_t: A_strchr_String(s, c), \
-))
+    str_t:    A_strchr_Str, \
+    string_t: A_strchr_String \
+))((s), (c))
 
 size_t A_strrchr_Str(str_t s, char c);
 size_t A_strrchr_String(string_t s, char c);
 
 #define A_strrchr(s, c) (_Generic((s), \
-    str_t:    A_strrchr_Str(s, c), \
-    string_t: A_strrchr_String(s, c), \
-))
+    str_t:    A_strrchr_Str, \
+    string_t: A_strrchr_String \
+))((s), (c))
+
+bool A_strcont_Str(str_t s, char c);
+bool A_strcont_String(string_t s, char c);
+
+#define A_strcont(s, c) (_Generic((s), \
+    str_t:    A_strcont_Str, \
+    string_t: A_strcont_String \
+))((s), (c))
 
 bool A_streq_Str_Str(str_t a, str_t b);
 bool A_streq_Str_String(str_t a, string_t b);
 bool A_streq_String_Str(string_t a, str_t b);
 bool A_streq_String_String(string_t a, string_t b);
 
-#define A_streq_Str(a, b) (_Generic((b), \
-    str_t:    A_streq_Str_Str(a, b), \
-    string_t: A_streq_Str_String(a, b), \
-))
-
-#define A_streq_String(a, b) (_Generic((b), \
-    str_t:    A_streq_String_Str(a, b), \
-    string_t: A_streq_String_String(a, b), \
-))
-
 #define A_streq(a, b) (_Generic((a), \
-    str_t:    A_streq_Str(a, b), \
-    string_t: A_streq_String(a, b), \
-))
+    str_t:    _Generic((b), \
+        str_t:    A_streq_Str_Str, \
+        string_t: A_streq_Str_String), \
+    string_t: _Generic((b), \
+        str_t:    A_streq_String_Str, \
+        string_t: A_streq_String_String) \
+))((a), (b))
 
-bool A_strpbrk_Str_Str(str_t a, str_t b);
-bool A_strpbrk_Str_String(str_t a, string_t b);
-bool A_strpbrk_String_Str(string_t a, str_t b);
-bool A_strpbrk_String_String(string_t a, string_t b);
-
-#define A_strpbrk_Str(a, b) (_Generic((b), \
-    str_t:    A_strpbrk_Str_Str(a, b), \
-    string_t: A_strpbrk_Str_String(a, b), \
-))
-
-#define A_strpbrk_String(a, b) (_Generic((b), \
-    str_t:    A_strpbrk_String_Str(a, b), \
-    string_t: A_strpbrk_String_String(a, b), \
-))
+size_t A_strpbrk_Str_Str(str_t a, str_t b);
+size_t A_strpbrk_Str_String(str_t a, string_t b);
+size_t A_strpbrk_String_Str(string_t a, str_t b);
+size_t A_strpbrk_String_String(string_t a, string_t b);
 
 #define A_strpbrk(a, b) (_Generic((a), \
-    str_t:    A_strpbrk_Str(a, b), \
-    string_t: A_strpbrk_String(a, b), \
-))
+    str_t:    _Generic((b), \
+        str_t:    A_strpbrk_Str_Str, \
+        string_t: A_strpbrk_Str_String), \
+    string_t: _Generic((b), \
+        str_t:    A_strpbrk_String_Str, \
+        string_t: A_strpbrk_String_String) \
+))((a), (b))
 
-bool A_strstr_Str_Str(str_t a, str_t b);
-bool A_strstr_Str_String(str_t a, string_t b);
-bool A_strstr_String_Str(string_t a, str_t b);
-bool A_strstr_String_String(string_t a, string_t b);
+size_t A_strrpbrk_Str_Str(str_t a, str_t b);
+size_t A_strrpbrk_Str_String(str_t a, string_t b);
+size_t A_strrpbrk_String_Str(string_t a, str_t b);
+size_t A_strrpbrk_String_String(string_t a, string_t b);
 
-#define A_strstr_Str(a, b) (_Generic((b), \
-    str_t:    A_strstr_Str_Str(a, b), \
-    string_t: A_strstr_Str_String(a, b), \
-))
+#define A_strrpbrk(a, b) (_Generic((a), \
+    str_t:    _Generic((b), \
+        str_t:    A_strrpbrk_Str_Str, \
+        string_t: A_strrpbrk_Str_String), \
+    string_t: _Generic((b), \
+        str_t:    A_strrpbrk_String_Str, \
+        string_t: A_strrpbrk_String_String) \
+))((a), (b))
 
-#define A_strstr_String(a, b) (_Generic((b), \
-    str_t:    A_strstr_String_Str(a, b), \
-    string_t: A_strstr_String_String(a, b), \
-))
+size_t A_strpcnt_Str_Str(str_t a, str_t b);
+size_t A_strpcnt_Str_String(str_t a, string_t b);
+size_t A_strpcnt_String_Str(string_t a, str_t b);
+size_t A_strpcnt_String_String(string_t a, string_t b);
+
+#define A_strpcnt(a, b) (_Generic((a), \
+    str_t:    _Generic((b), \
+        str_t:    A_strpcnt_Str_Str, \
+        string_t: A_strpcnt_Str_String), \
+    string_t: _Generic((b), \
+        str_t:    A_strpcnt_String_Str, \
+        string_t: A_strpcnt_String_String) \
+))((a), (b))
+
+size_t A_strrpcnt_Str_Str(str_t a, str_t b);
+size_t A_strrpcnt_Str_String(str_t a, string_t b);
+size_t A_strrpcnt_String_Str(string_t a, str_t b);
+size_t A_strrpcnt_String_String(string_t a, string_t b);
+
+#define A_strrpcnt(a, b) (_Generic((a), \
+    str_t:    _Generic((b), \
+        str_t:    A_strrpcnt_Str_Str, \
+        string_t: A_strrpcnt_Str_String), \
+    string_t: _Generic((b), \
+        str_t:    A_strrpcnt_String_Str, \
+        string_t: A_strrpcnt_String_String) \
+))((a), (b))
+
+size_t A_strstr_Str_String(str_t a, string_t b);
+size_t A_strstr_String_Str(string_t a, str_t b);
+size_t A_strstr_String_String(string_t a, string_t b);
 
 #define A_strstr(a, b) (_Generic((a), \
-    str_t:    A_strstr_Str(a, b), \
-    string_t: A_strstr_String(a, b), \
-))
+    str_t:    _Generic((b), \
+        str_t:    A_strstr_Str_Str, \
+        string_t: A_strstr_Str_String), \
+    string_t: _Generic((b), \
+        str_t:    A_strstr_String_Str, \
+        string_t: A_strstr_String_String) \
+))((a), (b))
+
+size_t A_strrstr_Str_Str(str_t a, str_t b);
+size_t A_strrstr_Str_String(str_t a, string_t b);
+size_t A_strrstr_String_Str(string_t a, str_t b);
+size_t A_strrstr_String_String(string_t a, string_t b);
+
+#define A_strrstr(a, b) (_Generic((a), \
+    str_t:    _Generic((b), \
+        str_t:    A_strrstr_Str_Str, \
+        string_t: A_strrstr_Str_String), \
+    string_t: _Generic((b), \
+        str_t:    A_strrstr_String_Str, \
+        string_t: A_strrstr_String_String) \
+))((a), (b))
+
+str_t A_substr_Str(str_t s, size_t i);
+str_t A_substr_String(string_t s, size_t i);
+
+#define A_substr(s, i) (_Generic((s), \
+    str_t:    A_substr_Str, \
+    string_t: A_substr_String \
+))((s), (i))
+
+string_t A_substring_Str(str_t s, size_t i);
+string_t A_substring_String(string_t s, size_t i);
+
+#define A_substring(s, i) (_Generic((s), \
+    str_t:    A_substring_Str, \
+    string_t: A_substring_String \
+))((s), (i))
